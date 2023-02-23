d4df93a82d966afedc156b837eee50da581b1ce4
Eronini Kelechi <kelechieronini@yahoo.com>
2023-02-23 15:41:05 +0100

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -5,7 +5,7 @@ const key = 'HlfXg5G1HslXfpOZh2x9SjBtOJ0aQTIq'
 
 const getWeather = async (id) =>{
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`
 
     const response = await fetch(base + query);
@@ -20,7 +20,7 @@ const getWeather = async (id) =>{
 // Get city conditions
 const getCity = async (city) =>{
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query =  `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
